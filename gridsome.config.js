8225bed602c4fd481868068b02b3b7785a992d28
Javid Izadfar <javid.izadfar@gmail.com>
2021-02-03 15:07:47 +0330

    first commit

---
+++ gridsome.config.js
@@ -0,0 +1,21 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'data/brands/*.json',
+        typeName: 'Brand',
+      }
+    },
+    {
+      use: "gridsome-plugin-tailwindcss",
+    },
+  ],
+}
