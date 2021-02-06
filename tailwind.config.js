b6f0a311a0baaab7b18d258f975aa6cfbae3578a
Javid Izadfar <javid.izadfar@gmail.com>
2021-02-06 23:09:36 +0330

    added basic colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
             fontFamily: {
                 sans: ['Vazir', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                rb: {
+                    indigo: {
+                        500: '#421196',
+                    },
+                    violet: {
+                        100: '#F5eBFF',
+                        500: '#9529FF',
+                        600: '#850AFF',
+                        900: '#463C74',
+                    },
+                    yellow: {
+                        500: '#FFC719',
+                    },
+                    red: {
+                        500: '#FF193B',
+                    },
+                    carnelian: {
+                        500: '#B8001C',
+                    },
+                },
+            },
         },
     },
 };
