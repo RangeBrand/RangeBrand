bb135eed60f0aaf96ffe737a3b82bd563a8bd3c8
Javid Izadfar <javid.izadfar@gmail.com>
2021-05-04 21:41:41 +0430

    purge unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
+    purge: [
+        './src/**/*.css',
+        './src/**/*.vue',
+    ],
     theme: {
         extend: {
             fontFamily: {
