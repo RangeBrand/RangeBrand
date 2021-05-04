7b48c7456a808c52865b021b7853377a8ede91dd
Javid Izadfar <javid.izadfar@gmail.com>
2021-05-04 16:38:30 +0430

    added about us

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,24 @@
 <template>
     <Layout>
-        <h1>About us</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+        <div class="container mx-auto px-4">
+            <main class="py-4 text-black text-opacity-80">
+                <h1 class="font-bold text-3xl my-4 text-rb-violet-500">
+                    درباره‌ی ما
+                </h1>
+                <hr class="border-gray-200 my-4">
+                <p class="mb-4">
+                    رنگ برند ابزاری برای مطالعه و انتخاب رنگ مناسب برای برندها است.
+                </p>
+                <p class="mb-4">
+                    رنگ برند را توسط جاوید ایزدفر به کمک ستاره کریمی و <a href="http://">مهرنوش دشتی</a> به عنوان یک پروژه‌ی متن باز در سال ۱۳۹۵ <a href="https://github.com/IKAcc/RangeBrand" rel="noreferrer noopener" class="visible-link">شروع شد</a>. همکنون نیز همچنان نگه‌داری از وبسایت و بروزرسانی داده‌ها به صورت مشارکتی و از طریق <a href="https://github.com/RangeBrand" rel="noreferrer noopener" class="visible-link">گیتهاب</a> انجام می‌شود.
+                </p>
+                <p class="mb-4">
+                    در حال حاضر، خدمتی که رنگ برند به کاربران خود ارائه می‌دهد، <g-link to="/brands/" class="visible-link">
+                        لیستی از برندهای مطرح در ایران
+                    </g-link> به همراه رنگ‌های رسمی آ‌ن‌هاست. به کمک این لیست می‌توان به عنوان مثال، رنگ‌های رسمی شبکه‌های اجتماعی را به سرعت پیدا کرد و در پروژه‌ها استفاده کرد.
+                </p>
+            </main>
+        </div>
     </Layout>
 </template>
 
