25e8cf141af76e2f62385aab5a4ddaac96c1d333
Javid Izadfar <javid.izadfar@gmail.com>
2021-05-07 20:08:12 +0430

    added move mechanism

--- src/components/color/banner.vue
+++ src/components/color/banner.vue
@@ -1,16 +1,30 @@
 <template>
-    <ul class="flex h-full">
+    <ul class="flex h-full relative overflow-hidden"
+        @mousemove="move"
+        @mouseup="captureOff">
         <li v-for="(color, index) in localColors"
             :key="color"
-            class="flex-grow relative">
-            <div class="color"
-                 :class="isLight(color) ? 'color--light' : 'color--dark'"
-                 :style="{backgroundColor: `#${color}`}">
+            class="flex-grow smooth-transition">
+            <div :class="[
+                     'color',
+                     isLight(color) ? 'color--light' : 'color--dark',
+                     {
+                         'z-50': captureToggle === color
+                     }
+                 ]"
+                 :style="{
+                     backgroundColor: `#${color}`,
+                     left: captureToggle === color ? `${x}px` : ''
+                 }">
                 <div class="color__actions flex-center smooth-transition">
                     <span class="p-2">
                         <icon-delete v-show="localColors.length > 2"
                                      class="color__actions__icon smooth-transition"
                                      @click="deleteColor(color)"/>
+                    </span>
+                    <span class="p-2">
+                        <icon-move class="color__actions__icon smooth-transition"
+                                   @mousedown="captureOn($event, color)"/>
                     </span>
                     <span class="p-2">
                         <icon-copy class="color__actions__icon smooth-transition"
@@ -20,14 +34,14 @@
                 <code
                     class="color__code smooth-transition"
                     dir="ltr">
-                    {{ color }}
+                    {{ color }} {{ index }}
                 </code>
-            </div>
-            <div class="color__add smooth-transition">
-                <div class="flex-center h-full">
-                    <span class="button" @click="addColor(index)">
-                        <icon-add class="w-8 fill-current"/>
-                    </span>
+                <div class="color__add smooth-transition">
+                    <div class="flex-center h-full">
+                        <span class="button" @click="addColor(index)">
+                            <icon-add class="w-8 fill-current"/>
+                        </span>
+                    </div>
                 </div>
             </div>
         </li>
@@ -36,6 +50,7 @@
 <script>
 import IconDelete from '~/assets/icons/delete.svg';
 import IconCopy from '~/assets/icons/copy.svg';
+import IconMove from '~/assets/icons/move.svg';
 import IconAdd from '~/assets/icons/add.svg';
 
 import { isLight } from '~/scripts/utils/luminance';
@@ -46,6 +61,7 @@ export default {
     components: {
         IconDelete,
         IconCopy,
+        IconMove,
         IconAdd,
     },
     mixins: [
@@ -60,7 +76,14 @@ export default {
     data() {
         return {
             localColors: this.colors,
+            captureToggle: null,
+            x: 0,
         };
+    },
+    computed: {
+        colorWidth() {
+            return window ? window.outerWidth / this.localColors.length : 0;
+        },
     },
     methods: {
         isLight,
@@ -74,6 +97,28 @@ export default {
                 return Math.round((item + rightColor[index]) / 2);
             });
             this.localColors.splice(index, 0, RGBtoHEX(newColor));
+        },
+        move(evt) {
+            if (this.captureToggle !== null) {
+                const color = this.localColors.indexOf(this.captureToggle);
+                const colorNo = this.localColors.length - color - 1;
+                // move color with mouse
+                this.x = evt.x - (colorNo * this.colorWidth) - (this.colorWidth / 2);
+
+                // replace positions of color
+                const distant = Math.round(this.x / this.colorWidth);
+                if (Math.abs(distant) !== 0) {
+                    [this.localColors[color], this.localColors[color - distant]] = [this.localColors[color - distant], this.localColors[color]];
+                }
+            }
+        },
+        captureOn(evt, color) {
+            this.captureToggle = color;
+            this.move(evt);
+        },
+        captureOff() {
+            this.captureToggle = null;
+            this.x = 0;
         },
     },
 };
@@ -109,7 +154,7 @@ export default {
     @apply hover:bg-opacity-50
 }
 .color__code {
-    @apply absolute bottom-0 right-0 left-0 text-center text-xl p-3 font-semibold uppercase;
+    @apply absolute bottom-0 right-0 left-0 text-center text-xl p-3 font-semibold uppercase select-none;
 }
 
 li:first-of-type .color__add {
