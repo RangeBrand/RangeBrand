c2401c1cd07861a97f00641130efb2021e2cdd09
Javid Izadfar <javid.izadfar@gmail.com>
2026-09-08 13:32:09 +0330

    remove the website

--- src/layouts/Empty.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="font-sans bg-white">
-        <slot/>
-    </div>
-</template>
--- src/scripts/utils/validator.js
+++
@@ -1,8 +0,0 @@
-export const validateHex = (color) => {
-    const regex = /^[0-9A-F]{6}$/i;
-    try {
-        return regex.test(color.toUpperCase());
-    } catch {
-        return false;
-    }
-};
